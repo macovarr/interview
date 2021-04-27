853ef44967d068d1633e7f8bfa200f4f23c1295a
macovarr <covarrubiasmarco@yahoo.com>
2021-04-27 12:54:20 -0700

    is valid parenthesization

---
+++ is_valid_parenthesization.py
@@ -0,0 +1,12 @@
+def is_well_formed(s):
+    left_chars, lookup = [], {'(': ')', '{': '}', '[': ']'}
+    for c in s:
+        if c in lookup:
+            left_chars.append(c)
+        elif not left_chars or lookup[left_chars.pop()] != c:
+            return False
+    return not left_chars
+
+
+assert is_well_formed('(()][){}') == False
+assert is_well_formed('(()[]){}') == True
